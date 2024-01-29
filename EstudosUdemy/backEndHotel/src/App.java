c72e13a0f74533df14de73d39a6151ea2aa8b223
luizx <luizfelipe.ifpa.2022@gmail.com>
2024-01-29 19:16:24 -0300

    backEndHotel adicionado

--- EstudosUdemy/backEndHotel/src/App.java
+++ EstudosUdemy/backEndHotel/src/App.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
+import controller.Hotel;
+import entity.Register;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        Register[] register = new Register[10];
+
+        System.out.println("Quantos quartos voce deseja alugar?");
+            int quantityRooms = scan.nextInt();
+        
+        if (quantityRooms > Hotel.MAXIMUN_ROOMS) {
+            throw new IllegalArgumentException("Numero de quartos excedidos");   
+        }
+
+        for (int i = 1; i <= quantityRooms; i++){
+            System.out.println("Cadastro");
+        System.out.println("Nome: ");
+            String name = scan.next();
+        System.out.println("Email: ");
+            String email = scan.next();
+        System.out.println("CPF: ");
+            String cpf = scan.next();
+        System.out.println("Numero do Quarto: ");
+            int roomNumber = scan.nextInt();
+        
+        register[roomNumber] = new Register(name, email, cpf);
+
+        System.out.println("Cadastro Realizado!");
+        System.out.println(register[roomNumber].toString());
+        }
+    scan.close();
     }
 }
---
+++ EstudosUdemy/backEndHotel/src/controller/Hotel.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Hotel {
+
+    public static final int MAXIMUN_ROOMS = 10;
+
+}
