1255904986fc3bbc903d13a2468f8ca373e03712
luizx <luizfelipe.ifpa.2022@gmail.com>
2024-02-04 14:13:28 -0300

    [ready]: function (Decimal for binary) and (Binary for Decimal)

--- CalculatorBinary/FrontEnd/index.js
+++ CalculatorBinary/FrontEnd/index.js
@@ -1,10 +1,24 @@
+//@Author: Luiz Felipe
+//@GitHub: https://github.com/LuizxxDev/Estudos-Java/tree/main/CalculatorBinary/FrontEnd
 
 const DIVIDER = 2;
 
-function convertForBinary(){
-    var decimalInput = document.getElementById("decimalInput").value;
-    var resultadoElement = document.getElementById("resultado");
+let currentFunction = convertForBinary;
 
+function toggleFunction(){
+    var resultadoElement = document.getElementById("resultado");
+    var titleElement = document.getElementById("title-calculator");
+    
+    currentFunction = (currentFunction === convertForBinary) ? BinaryForDecimal : convertForBinary;
+    
+    resultadoElement.textContent = "";
+
+    titleElement.textContent = (currentFunction === convertForBinary) ? "Decimal For Binary" : "Binary For Decimal";
+}
+
+function convertForBinary(){
+    var decimalInput = document.getElementById("Input").value;
+    var resultadoElement = document.getElementById("resultado");
     if(isNaN(decimalInput) || decimalInput === ""){
         alert('Por favor, insira um número decimal válido.');
             return;
@@ -20,7 +34,31 @@ function convertForBinary(){
     resultadoElement.textContent = binary.toString();
 }
 
+function BinaryForDecimal(){
+    
+    var decimalInput = document.getElementById("Input").value;
+    var resultadoElement = document.getElementById("resultado");
+
+    if(isNaN(decimalInput)){
+        alert("Por favor, insira um numero binario valido");
+    }
+    debugger;
+
+    var arrayBinary = decimalInput.split('');
+    var result = 0;
+
+    for(let i = arrayBinary.length - 1; i >= 0; i--){
+        result += parseInt(arrayBinary[i] * Math.pow(2, arrayBinary.length - 1 - i))
+        resultadoElement.textContent = result.toString();
+    }
+}
+
 function limpar(){
     var resultadoElement = document.getElementById("resultado");
     resultadoElement.textContent = " ";
 }
+
+function performConversion() {
+    currentFunction();
+}
+
